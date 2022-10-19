9717a5199bb9ab2585ecc69c7f3113401fa99491
distepstar <jobsdzihinss@gmail.com>
2022-10-19 01:02:50 -0400

    lsp format cofig fix

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -7,7 +7,7 @@ end
 local lsp_formatting = function(bufnr)
   vim.lsp.buf.format({
     filter = function(client)
-      return client.name == 'prettier'
+      return client.name == 'null-ls'
     end,
     bufnr = bufnr,
   })
